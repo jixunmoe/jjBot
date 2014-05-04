62d31c2f4084e7fd4f73ae3c593380ef1e712106
JixunMoe <Jixun.Moe@Gmail.Com>
2014-05-04 23:08:29 +0100

    Fixes Typo & round down the time.

--- mod.d/99.boot.js
+++ mod.d/99.boot.js
@@ -1,5 +1,6 @@
 /* jslint node:true */
 
+// 计算启动时间用到。
 module.exports = function (conf, mod) {
 	return {
 		time: new Date()
--- plug.d/10.uptime.js
+++ plug.d/10.uptime.js
@@ -9,14 +9,14 @@ pluginHello.prototype = {
 	name  : 'Uptime',
 	ver   : '1.0',
 	author: 'Jixun',
-	desc  : '获取机器人上线事件长度',
+	desc  : '获取机器人上线时间长度',
 	load: function () {
 		var that = this;
 		
 		// 安裝 Hook
 		this.regEvent ('msg-cmd-uptime', function (reply) {
 			var secs = (new Date() - that.bot.mod.boot.time) / 1000;
-			reply ('这次欢快的跑了 ' + secs % 86400 / 3600 + '时 ' + secs % 3600 / 60 + '分钟呢¬');
+			reply ('这次欢快的跑了 ' + Math.floor(secs % 86400 / 3600) + '时 ' + Math.floor(secs % 3600 / 60) + '分钟呢~');
 		});
 	},
 	unload: function () {
