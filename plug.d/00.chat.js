dfbc7beff0f3c607b0825faf4713caac3be3e521
NijiharaTsubasa <nico@aji.moe>
2015-01-06 12:16:29 +0800

    增加插件（未适配当前版本）

---
+++ plug.d/00.chat.js
@@ -0,0 +1,182 @@
+/*jslint node: true*/
+
+var pluginChat = function (Bot, regEvent) {
+	this.bot = Bot;
+	this.mod = Bot.mod;
+	this.regEvent = regEvent;
+	this.ext = Bot.mod.db;
+	this.db = this.ext.db;
+	this.disabled=false;
+	this.count=0;
+	Bot.mod.log.info ('Init. database ...');
+	this.db.query (this.ext._(this.ext.__(function () {/*
+	create table if not exists `jB_chat` (
+		`ask` TEXT NOT NULL,
+		`answer` TEXT NOT NULL
+	)ENGINE = %s DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+	*/}), this.ext.conf.engine));
+};
+
+/* 本模组的config，请添加到config.yaml
+
+# 聊天插件设置
+chat:
+# 我的称呼
+	name: 'jjBot'
+# 群组中的回复率
+	answerRate: 0.8
+# 是否接收禁用 / 启用命令
+	recieveDisable: false
+# 禁用 / 启用命令
+	disableCmd: '闭嘴'
+	enableCmd: '张嘴'
+# 禁用 / 启用命令是否需要jj/前缀
+	usePrefixToDisable: false
+# 禁用/启用所需次数
+	disableCount: 3
+# 管理员QQ号列表：可以无视上面的次数【TODO】
+	directDisable:
+		-10000
+# 是否允许教学
+	allowTeach: true
+# 教学指令 (需要jj前缀)
+	teachCommand: 'ask'
+# 教学指令中问与答的分隔符
+	teachSeparator: 'answer'
+
+*/
+
+function SelectBestAnswer(data) {
+	var key=[];
+	var answer=[];
+	for(var i in data) { //关键词和回复的初始权重
+		if(!key[data[i].ask]) key[data[i].ask]=data[i].ask.length*2; //关键词的初始权重为字数*2
+		else key[data[i].ask]++; //被教过一次+1
+		if(!answer[data[i].answer]) answer[data[i].answer]=0; 
+		answer[data[i].answer]+=data[i].ask.length*2; //把关键词的初始权重赋给回复作为初始值
+	}
+	for(var ans in answer) {
+		for(var ask in key) { //检测回复中出现的所有关键词（包括重复的）
+			eval("var cnt=ans.match(/"+ask+"/g)");
+			if(cnt!=null)
+				answer[ans]+=(cnt.length*key[ask]); //回复的权重加上(关键词权重*出现次数)
+		}
+	}
+	answer.sort(function(a,b) {if (x > y) return -1; else  return 1;});
+	var ret=[];
+	var lastWeight=0;
+	for(var ans in answer) { //返回5条权重最高的，相同的都返回
+		if(ret.length>5 && lastWeight>answer[ans]) break;
+		ret.push(ans);
+		lastWeight=answer[ans];
+	}
+	return ret;
+}
+
+
+pluginChat.prototype = {
+	name  : '聊天',
+	ver   : '0.2',
+	author: 'lyh',
+	desc  : '根据收到的聊天信息给出回复。依赖 db 模组。',
+	disable: function(reply) {
+		if(!this.disabled) {
+			this.count++;
+			if(this.count==this.conf.disableCount) {
+				reply(this.conf.name+'已停用，发送 '+(this.conf.usePrefixToDisable?(this.bot.conf.cmdPrefix+'/'):'')+this.conf.enableCmd+' 来启用我');
+				this.disabled=true;
+				this.count=0;
+			}else reply('已有'+this.count+'人请求停用'+this.conf.name+'，还需要 '+(this.conf.disableCount-this.count)+' 人请求才会执行~');
+		}
+	},
+	enable: function(reply) {
+		if(this.disabled) {
+			this.count++;
+			if(this.count==this.conf.disableCount) {
+				reply(this.conf.name+'已启用，发送 '+(this.conf.usePrefixToDisable?(this.bot.conf.cmdPrefix+'/'):'')+this.conf.disableCmd+' 来停用我');
+				this.disabled=false;
+				this.count=0;
+			}else reply('已有'+this.count+'人请求启用'+this.conf.name+'，还需要 '+(this.conf.disableCount-this.count)+' 人请求才会执行~');
+		}
+	},
+	load: function () {
+		if(typeof(this.bot.conf.chat)!='undefined')
+			this.conf=this.bot.conf.chat;
+		else {
+			this.conf=new Object();
+			//载入缺省配置
+			this.conf.name= 'jjBot';
+			this.conf.answerRate= 0.8;
+			this.conf.recieveDisable= false; //太容易触发禁言
+			this.conf.disableCmd= '闭嘴';
+			this.conf.enableCmd= '张嘴';
+			this.conf.usePrefixToDisable= false;
+			this.conf.disableCount= 3;
+			this.conf.directDisable={};
+			this.conf.allowTeach= true;
+			this.conf.teachCommand= 'ask';
+			this.conf.teachSeparator= 'answer';
+		}
+		
+		var that = this;
+		if(that.conf.recieveDisable) {
+			if(that.conf.usePrefixToDisable) {
+				// 设置命令说明文本
+				that.regEvent ('initCmdDesc', function () {
+					that.bot.Plugin.on('setCmdDesc',that.conf.disableCmd,'让我闭嘴');
+					that.bot.Plugin.on('setCmdDesc',that.conf.enableCmd,'让我继续聊天');
+				});
+				// 安装 Hook
+				that.regEvent ('msg-cmd-'+that.conf.disableCmd, that.disable(reply));
+				that.regEvent ('msg-cmd-'+that.conf.enableCmd, that.enable(reply));
+			} else {
+				that.regEvent ('msg', function (content, msg, reply) {
+					if(content.replace(/(^\s*)|(\s*$)/g,"")==that.conf.disableCmd) {
+						that.disable(reply);
+					}
+					if(content.replace(/(^\s*)|(\s*$)/g,"")==that.conf.enableCmd)
+						that.enable(reply);
+				});
+			}
+		}
+		
+		if(!that.disabled) {
+			if(that.conf.allowTeach) {
+				that.regEvent ('initCmdDesc', function () {
+					that.bot.Plugin.on('setCmdDesc',that.conf.teachCommand,'关键词 '+that.conf.teachSeparator+' 回答','教我说话（目前暂时不能在回答中加入称呼等动态内容，今后会改进）');
+				});
+				that.regEvent('msg-cmd-'+that.conf.teachCommand, function (reply, msg, args) {
+					var str=args.join(' ');
+					str=str.split(' answer ');
+					if(!str[1] || str[2]) {
+						reply('请按照 '+that.bot.conf.cmdPrefix+'/'+that.conf.teachCommand+' 收到的内容 '+that.conf.teachSeparator+' 回答的内容 的格式来教我说话哦');
+						return true;
+					}
+					that.db.query ('INSERT INTO `jB_chat` VALUES (?,?);', [str[0],str[1]], function () {
+					reply (str[1]);
+					});
+				});
+			}
+		}
+		that.regEvent('msg',function (str,msg,reply) {
+			if(!that.disabled) {
+				if(!msg.isGroup || Math.random()<=that.conf.answerRate) {
+					that.db.query ('select * from `jB_chat` where ? like concat(\'%\',ask,\'%\')', str, function (err, data) {
+						if (data.length) {
+							data=SelectBestAnswer(data);
+							data.sort(function() {return 0.5-Math.random(); });
+							reply(data[0]);
+						}
+					});
+				}
+			}
+		});
+	},
+	unload: function () {
+		this.disabled=false;
+		this.count=0;
+		this.conf={};
+	}
+};
+
+module.exports = pluginChat;
---
+++ plug.d/01.help.js
@@ -0,0 +1,84 @@
+/*jslint node: true*/
+
+var sprintf = require('util').format;
+var pluginHelp = function (Bot, regEvent) {
+	this.bot = Bot;
+	this.regEvent = regEvent;
+	this.listener = Bot.Plugin.listener;
+	this.plugins = Bot.Plugin.plugins;
+	this.cmdDesc = {};
+	this.cmdArgs = {};
+	this.setCmdDesc=function(cmd,args,desc) {
+		if(arguments.length!=2 && arguments.length!=3)
+			return;
+		if(arguments.length==2) {
+			this.setCmdDesc.that.cmdDesc[cmd]=args;
+		}
+		else {
+			this.setCmdDesc.that.cmdArgs[cmd]=args;
+			this.setCmdDesc.that.cmdDesc[cmd]=desc;
+		}
+	}
+	this.setCmdDesc.that=this;
+};
+
+pluginHelp.prototype = {
+	name  : '指令列表',
+	ver   : '1.0',
+	author: 'lyh',
+	desc  : '通过指令 help 显示指令列表，指令 plugins 显示插件列表',
+	load: function () {
+		var that=this;
+		that.regEvent ('setCmdDesc', that.setCmdDesc);
+		this.bot.Plugin.on('initCmdDesc');
+		// 设置命令说明文本
+		this.regEvent ('initCmdDesc', function () {
+			that.bot.Plugin.on('setCmdDesc','help','显示本说明文本');
+			that.bot.Plugin.on('setCmdDesc','plugins','显示安装的插件列表');
+			//jjBot内置插件
+			that.bot.Plugin.on('setCmdDesc','sign','签到，可以获得'+that.bot.conf.user.currency);
+			that.bot.Plugin.on('setCmdDesc','money','查看'+that.bot.conf.user.currency+'的数目');
+			that.bot.Plugin.on('setCmdDesc','nick','设置昵称');
+			that.bot.Plugin.on('setCmdDesc','top','查看'+that.bot.conf.user.currency+'余额排行榜');
+			that.bot.Plugin.on('setCmdDesc','pem',false); //在指令列表中隐藏这条指令
+			that.bot.Plugin.on('setCmdDesc','pay','用户QQ号 转账数目','转账');
+			that.bot.Plugin.on('setCmdDesc','uptime','查看机器人运行时间');
+			that.bot.Plugin.on('setCmdDesc','fly','jj fly~');
+			that.bot.Plugin.on('setCmdDesc','ping','演示用指令');
+			that.bot.Plugin.on('setCmdDesc','prpr','[谁] [什么部位]','舔一舔');
+			that.bot.Plugin.on('setCmdDesc','time','报时');
+			that.bot.Plugin.on('setCmdDesc','img',false);
+		});
+		// 安裝 Hook
+		this.regEvent ('msg-cmd-help', function (reply, msg) {
+			rp='当前支持的指令列表如下：\n';
+			for(var type in that.listener) {
+				if(type.substr(0,8)=='msg-cmd-') {
+					var cmdName=type.substr(8);
+					if(that.cmdDesc[cmdName]==false)
+						continue;
+					rp+=that.bot.conf.cmdPrefix+'/'+cmdName;
+					if(typeof(that.cmdArgs[cmdName])!="undefined")
+						rp+=' '+that.cmdArgs[cmdName];
+					if(typeof(that.cmdDesc[cmdName])!="undefined")
+						rp+='： '+that.cmdDesc[cmdName];
+					rp+='\n';
+				}
+			}
+			reply(rp);
+		});
+		this.regEvent ('msg-cmd-plugins', function (reply, msg) {
+			rp='当前已安装的插件列表如下：\n';
+			for(var key in that.plugins) {
+				rp+=that.plugins[key].name+' '+that.plugins[key].ver+' 作者：'+that.plugins[key].author+' '+that.plugins[key].desc+'\n';
+			}
+			reply(rp);
+		});
+	},
+	unload: function () {
+		this.cmdDesc = {};
+		this.cmdArgs = {};
+	}
+};
+
+module.exports = pluginHelp;
